68d51b0164dde89c97778593c805a0f9a4846408
Danny-1104 <daparedes21@espe.edu.ec>
2026-01-29 20:27:46 -0500

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -1,7 +1,7 @@
 <?php
 // app/config/database.php
-define('DB_SERVIDOR', 'localhost');
-define('DB_USUARIO', 'root');
-define('DB_PASSWORD', '');   // 👈 VACÍO
-define('DB_NOMBRE', 'gestion_canchas');
+define('DB_SERVIDOR', 'db.fr-pari1.bengt.wasmernet.com');
+define('DB_USUARIO', 'c07611ae70648000d2d117ae244f');
+define('DB_PASSWORD', '0697c076-11ae-7191-8000-de39b1e6456f');   // 👈 VACÍO
+define('DB_NOMBRE', 'dbVpXBRYcXWMAsLf3MgEYGdc');
 define('DB_CHARSET', 'utf8mb4');
